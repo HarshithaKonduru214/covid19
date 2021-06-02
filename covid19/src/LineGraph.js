1069f1be21731ca8f1b7a5978ca73314b4dc158b
HarshithaKonduru214 <harshitakonduru2244@gmail.com>
2021-06-02 20:46:52 +0530

    feat: line graph added

---
+++ covid19/src/LineGraph.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+
+const buildChartData = (data, casesType) => {
+  let chartData = [];
+  let lastDataPoint;
+  for (let date in data.cases) {
+    if (lastDataPoint) {
+      let newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint,
+      };
+      chartData.push(newDataPoint);
+    }
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+};
+
+function LineGraph({ casesType }) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          let chartData = buildChartData(data, casesType);
+          setData(chartData);
+          console.log(chartData);
+          // buildChart(chartData);
+        });
+    };
+
+    fetchData();
+  }, [casesType]);
+
+  return (
+    <div className="linegraph">
+      {data?.length > 0 && (
+        <Line
+          data={{
+            datasets: [
+              {
+                backgroundColor: "black",
+                borderColor: "black",
+                data: data,
+              },
+            ],
+          }}
+          
+        />
+      )}
+    </div>
+  );
+}
+
+export default LineGraph;
